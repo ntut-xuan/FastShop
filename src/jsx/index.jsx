c22224184e117b9b6906000ee8f85094c05d0738
ntut-xuan <t109590031@ntut.org.tw>
2022-09-23 15:43:05 +0000

    Add tailwindcss config, add front-end essentials for index page

---
+++ src/jsx/index.jsx
@@ -0,0 +1,8 @@
+class App extends React.Component {
+    render(){
+        return <p>Hello World!</p>
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app"))
+root.render(<App />)
