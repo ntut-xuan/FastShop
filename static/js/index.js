f5ecb64a5a5326975dc9e25597f4ab07084f9f2b
Uriah Xuan <t109590031@ntut.org.tw>
2022-10-06 21:47:16 +0800

    Finish dynamic layout in index (#24)

--- static/js/index.js
+++ static/js/index.js
@@ -9,28 +9,45 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 var NevigationBar = function (_React$Component) {
     _inherits(NevigationBar, _React$Component);
 
-    function NevigationBar() {
+    function NevigationBar(props) {
         _classCallCheck(this, NevigationBar);
 
-        return _possibleConstructorReturn(this, (NevigationBar.__proto__ || Object.getPrototypeOf(NevigationBar)).apply(this, arguments));
+        var _this = _possibleConstructorReturn(this, (NevigationBar.__proto__ || Object.getPrototypeOf(NevigationBar)).call(this, props));
+
+        _this.nav_extend_on = _this.nav_extend_on.bind(_this);
+        _this.nav_extend_off = _this.nav_extend_off.bind(_this);
+        return _this;
     }
 
     _createClass(NevigationBar, [{
+        key: "nav_extend_on",
+        value: function nav_extend_on() {
+            document.getElementById("nav_extend_main").classList.remove("h-0");
+            document.getElementById("nav_extend_main").classList.add("h-80");
+        }
+    }, {
+        key: "nav_extend_off",
+        value: function nav_extend_off() {
+            document.getElementById("nav_extend_main").classList.remove("h-80");
+            document.getElementById("nav_extend_main").classList.add("h-0");
+        }
+    }, {
         key: "render",
         value: function render() {
-            return React.createElement(
+            var clickable_text = "h-full transition-all duration-200 hover:bg-gray-400 hover:text-white px-5 cursor-pointer";
+            return [React.createElement(
                 "div",
-                { className: "w-full h-200" },
+                { className: "w-full fixed top-0 left-0 z-20 bg-white" },
                 React.createElement(
                     "div",
-                    { className: "w-[90%] mx-auto h-20 flex flex-row p-5" },
+                    { className: "w-[90%] mx-auto h-20 flex flex-row relative left-0 top-0" },
                     React.createElement(
                         "div",
-                        { className: "w-full h-full  gap-10  flex-row flex justify-start" },
-                        React.createElement("img", { className: "h-full w-auto", src: "/static/image/fastshop.svg" }),
+                        { className: "w-full h-full gap-10 flex-row flex justify-start" },
+                        React.createElement("img", { className: "h-full w-auto py-5", src: "/static/image/fastshop.svg" }),
                         React.createElement(
                             "div",
-                            { className: "h-full" },
+                            { className: clickable_text },
                             React.createElement(
                                 "p",
                                 { className: "relative top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" },
@@ -39,7 +56,7 @@ var NevigationBar = function (_React$Component) {
                         ),
                         React.createElement(
                             "div",
-                            { className: "h-full" },
+                            { className: clickable_text, onMouseOver: this.nav_extend_on, onMouseOut: this.nav_extend_off },
                             React.createElement(
                                 "p",
                                 { className: "relative top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" },
@@ -48,7 +65,7 @@ var NevigationBar = function (_React$Component) {
                         ),
                         React.createElement(
                             "div",
-                            { className: "h-full" },
+                            { className: clickable_text },
                             React.createElement(
                                 "p",
                                 { className: "relative top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" },
@@ -57,7 +74,7 @@ var NevigationBar = function (_React$Component) {
                         ),
                         React.createElement(
                             "div",
-                            { className: "h-full" },
+                            { className: clickable_text },
                             React.createElement(
                                 "p",
                                 { className: "relative top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" },
@@ -67,10 +84,10 @@ var NevigationBar = function (_React$Component) {
                     ),
                     React.createElement(
                         "div",
-                        { className: "w-full h-full mx-auto gap-10 p-3 flex-row flex justify-end" },
+                        { className: "w-full h-full mx-auto gap-10 flex-row flex justify-end" },
                         React.createElement(
                             "div",
-                            { className: "h-full" },
+                            { className: clickable_text },
                             React.createElement(
                                 "p",
                                 { className: "relative top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" },
@@ -79,7 +96,7 @@ var NevigationBar = function (_React$Component) {
                         ),
                         React.createElement(
                             "div",
-                            { className: "h-full" },
+                            { className: clickable_text },
                             React.createElement(
                                 "p",
                                 { className: "relative top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" },
@@ -88,7 +105,7 @@ var NevigationBar = function (_React$Component) {
                         ),
                         React.createElement(
                             "div",
-                            { className: "h-full" },
+                            { className: clickable_text },
                             React.createElement(
                                 "p",
                                 { className: "relative top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" },
@@ -97,7 +114,63 @@ var NevigationBar = function (_React$Component) {
                         )
                     )
                 )
-            );
+            ), React.createElement(
+                "div",
+                { id: "nav_extend", className: "w-full fixed top-0 left-0 z-10 bg-white bg-opacity-90 flex flex-col" },
+                React.createElement("div", { id: "nav_extend_offset", className: "w-[90%] mx-auto h-20 flex flex-row relative left-0 top-0 transition-all duration-500" }),
+                React.createElement(
+                    "div",
+                    { id: "nav_extend_main", className: "w-[60%] mx-auto h-0 flex flex-row justify-start gap-5 relative left-0 top-0 transition-all duration-500 overflow-y-hidden", onMouseOver: this.nav_extend_on, onMouseOut: this.nav_extend_off },
+                    React.createElement(
+                        "div",
+                        { className: "p-5 text-center" },
+                        React.createElement(
+                            "p",
+                            { className: "text-2xl p-5" },
+                            " - \u9B54\u6CD5 - "
+                        ),
+                        React.createElement(
+                            "p",
+                            { className: "text-lg border-opacity-0 border-black border-b-2 hover:border-opacity-100 cursor-pointer my-3 py-2 duration-500" },
+                            " \u6642\u9593\u56DE\u6714 "
+                        ),
+                        React.createElement(
+                            "p",
+                            { className: "text-lg border-opacity-0 border-black border-b-2 hover:border-opacity-100 cursor-pointer my-3 py-2 duration-500" },
+                            " \u8CC7\u8A0A\u5B89\u5168 "
+                        ),
+                        React.createElement(
+                            "p",
+                            { className: "text-lg border-opacity-0 border-black border-b-2 hover:border-opacity-100 cursor-pointer my-3 py-2 duration-500" },
+                            " \u5730\u9707 "
+                        )
+                    ),
+                    React.createElement(
+                        "div",
+                        { className: "p-5 text-center" },
+                        React.createElement(
+                            "p",
+                            { className: "text-2xl p-5" },
+                            " - \u98DF\u7269 - "
+                        ),
+                        React.createElement(
+                            "p",
+                            { className: "text-lg border-opacity-0 border-black border-b-2 hover:border-opacity-100 cursor-pointer my-3 py-2 duration-500" },
+                            " \u5496\u5561 "
+                        ),
+                        React.createElement(
+                            "p",
+                            { className: "text-lg border-opacity-0 border-black border-b-2 hover:border-opacity-100 cursor-pointer my-3 py-2 duration-500" },
+                            " \u5361\u5E03\u5947\u8AFE "
+                        ),
+                        React.createElement(
+                            "p",
+                            { className: "text-lg border-opacity-0 border-black border-b-2 hover:border-opacity-100 cursor-pointer my-3 py-2 duration-500" },
+                            " \u62FF\u9435 "
+                        )
+                    )
+                )
+            )];
         }
     }]);
 
