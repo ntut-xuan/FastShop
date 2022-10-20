ce7a9ededfe4023dd7497b7105cf09bb3144f690
Lai-YT <381xvmvbib@gmail.com>
2022-10-20 14:32:38 +0000

    refactor: remove magic number & clean json sending

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,41 +1,26 @@
 from __future__ import annotations
-
-from typing import TYPE_CHECKING, no_type_check
-
-import pymysql
-import json
-
-from app import current_app
+from http import HTTPStatus
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from flask import Flask
+    from flask.testing import FlaskClient
+    from werkzeug.test import TestResponse
 
 
-@no_type_check
-def test_invalid_data_on_login(app: Flask) -> None:
-    with app.app_context():
-        test_client = current_app.test_client()
-        data = json.dumps(
-            {
-                "entroy": "orz",
-                "alice": "orz",
-                "lai-yt": "orz",
-                "jackson": "orz",
-                "uriah": "garbage",
-            }
-        )
-        resp = test_client.post(
-            "/login", data=data, headers={"content-type": "application/json"}
-        )
-        assert resp.status_code == 400
+def test_invalid_data_on_login_has_status_bad_request(client: FlaskClient) -> None:
+    invalid_data: dict[str, str] = {"uriah": "garbage"}
+
+    resp: TestResponse = client.post("/login", json=invalid_data)
+
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
 
 
-@no_type_check
-def test_invalid_email_on_login(app: Flask) -> None:
-    with app.app_context():
-        test_client = current_app.test_client()
-        data = json.dumps({"e-mail": "t109590031@ntut@org@tw", "password": "12345678"})
-        resp = test_client.post(
-            "/login", data=data, headers={"content-type": "application/json"}
-        )
-        assert resp.status_code == 422
+def test_invalid_email_on_login_has_status_unprocessable_entity(
+    client: FlaskClient,
+) -> None:
+    invalid_email: str = "t109590031@ntut@org@tw"
+    data: dict[str, str] = {"e-mail": invalid_email, "password": "12345678"}
+
+    resp: TestResponse = client.post("/login", json=data)
+
+    assert resp.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
