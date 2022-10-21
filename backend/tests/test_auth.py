7888d6e8bb1e75fe07d44b79706b57ebda535978
Lai-YT <381xvmvbib@gmail.com>
2022-10-21 10:14:51 +0000

    Refactor test with class group
    Gather the tests which aim the same function into a single class named
    by the function. The class works as a namespace, and class-specific
    fixtures can be used.

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -12,62 +12,68 @@ if TYPE_CHECKING:
     from werkzeug.test import TestResponse
 
 
-def test_get_register_should_response_content_of_register_html(
-    client: FlaskClient,
-) -> None:
-    resp: TestResponse = client.get("/register")
+class TestRegister:
+    def test_get_should_response_content_of_register_html(
+        self,
+        client: FlaskClient,
+    ) -> None:
+        resp: TestResponse = client.get("/register")
 
-    assert b"<!-- register.html (a marker for API test) -->" in resp.data
-
-
-def test_get_login_should_response_content_of_login_html(client: FlaskClient) -> None:
-    resp: TestResponse = client.get("/login")
-
-    assert b"<!-- login.html (a marker for API test) -->" in resp.data
+        assert b"<!-- register.html (a marker for API test) -->" in resp.data
 
 
-def test_post_login_with_invalid_data_should_have_code_bad_request(
-    client: FlaskClient,
-) -> None:
-    invalid_data: dict[str, str] = {"uriah": "garbage"}
+class TestLogin:
+    def test_get_should_response_content_of_login_html(
+        self,
+        client: FlaskClient,
+    ) -> None:
+        resp: TestResponse = client.get("/login")
 
-    resp: TestResponse = client.post("/login", json=invalid_data)
+        assert b"<!-- login.html (a marker for API test) -->" in resp.data
 
-    assert resp.status_code == HTTPStatus.BAD_REQUEST
+    def test_post_with_invalid_data_should_have_code_bad_request(
+        self,
+        client: FlaskClient,
+    ) -> None:
+        invalid_data: dict[str, str] = {"uriah": "garbage"}
 
+        resp: TestResponse = client.post("/login", json=invalid_data)
 
-def test_post_login_with_invalid_data_should_response_failed_message_in_json(
-    client: FlaskClient,
-) -> None:
-    invalid_data: dict[str, str] = {"uriah": "garbage"}
+        assert resp.status_code == HTTPStatus.BAD_REQUEST
 
-    resp: TestResponse = client.post("/login", json=invalid_data)
+    def test_post_with_invalid_data_should_response_failed_message_in_json(
+        self,
+        client: FlaskClient,
+    ) -> None:
+        invalid_data: dict[str, str] = {"uriah": "garbage"}
 
-    assert resp.is_json
-    assert resp.json is not None and resp.json["status"] == "Failed"
+        resp: TestResponse = client.post("/login", json=invalid_data)
 
+        assert resp.is_json
+        assert resp.json is not None and resp.json["status"] == "Failed"
 
-def test_post_login_with_invalid_email_should_have_code_unprocessable_entity(
-    client: FlaskClient,
-) -> None:
-    invalid_email: str = "t109590031@ntut@org@tw"
-    data: dict[str, str] = {"e-mail": invalid_email, "password": "12345678"}
+    def test_post_with_invalid_email_should_have_code_unprocessable_entity(
+        self,
+        client: FlaskClient,
+    ) -> None:
+        invalid_email: str = "t109590031@ntut@org@tw"
+        data: dict[str, str] = {"e-mail": invalid_email, "password": "12345678"}
 
-    resp: TestResponse = client.post("/login", json=data)
+        resp: TestResponse = client.post("/login", json=data)
 
-    assert resp.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+        assert resp.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
+    def test_post_with_invalid_email_should_response_failed_message_in_json(
+        self,
+        client: FlaskClient,
+    ) -> None:
+        invalid_email: str = "t109590031@ntut@org@tw"
+        data: dict[str, str] = {"e-mail": invalid_email, "password": "12345678"}
 
-def test_post_login_with_invalid_email_should_response_failed_message_in_json(
-    client: FlaskClient,
-) -> None:
-    invalid_email: str = "t109590031@ntut@org@tw"
-    data: dict[str, str] = {"e-mail": invalid_email, "password": "12345678"}
+        resp: TestResponse = client.post("/login", json=data)
 
-    resp: TestResponse = client.post("/login", json=data)
-
-    assert resp.is_json
-    assert resp.json is not None and resp.json["status"] == "Failed"
+        assert resp.is_json
+        assert resp.json is not None and resp.json["status"] == "Failed"
 
 
 some_invalid_emails: list[str] = ["plainaddress", "#@%^%#$@#$@#.com", "@example.com", "Joe Smith <email@example.com>", "email.example.com", "email@example@example.com", ".email@example.com", "email..email@example.com", "email@example.com (Joe Smith)", "email@example", "email@-example.com", "email@111.222.333.44444", "email@example..com", "Abc..123@example.com"]  # fmt: skip
