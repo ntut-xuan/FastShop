544215e91783c149e4aa049f8cb13556c8731c64
Lai-YT <381xvmvbib@gmail.com>
2022-10-22 15:40:46 +0000

    Fix missing table in testing database
    In-memory SQLite database is used in test environment, which opens
    a separate database for each connection. So once a connection is closed,
    all data are wiped out and tables are missed.

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import os
 import sqlite3
+import tempfile
 from pathlib import Path
 from typing import TYPE_CHECKING, Generator
 
@@ -15,22 +17,22 @@ if TYPE_CHECKING:
     from flask.testing import FlaskClient
 
 
-@pytest.fixture(autouse=True)
-def monkey_patch_to_use_sqlite_in_test(monkeypatch: pytest.MonkeyPatch) -> None:
-    def connect_sqlite_database() -> None:
-        g.db = sqlite3.connect(":memory:")
-
-    monkeypatch.setattr("database.util.connect_database", connect_sqlite_database)
-
-
 @pytest.fixture
-def app() -> Generator[Flask, None, None]:
+def app(monkeypatch: pytest.MonkeyPatch) -> Generator[Flask, None, None]:
+    db_fd, db_path = tempfile.mkstemp()
+    monkeypatch.setattr(
+        "database.util.connect_database",
+        lambda: connect_sqlite_database(db_path),
+    )
     app: Flask = create_app({"TESTING": True})
     with app.app_context():
         create_database()
         insert_test_data()
 
     yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
 
 
 @pytest.fixture
@@ -41,3 +43,7 @@ def client(app: Flask) -> FlaskClient:
 def insert_test_data() -> None:
     data_sql: str = (Path(__file__).parent / "data.sql").read_text("utf-8")
     get_database().cursor().executescript(data_sql)
+
+
+def connect_sqlite_database(db_path: str) -> None:
+    g.db = sqlite3.connect(db_path)
