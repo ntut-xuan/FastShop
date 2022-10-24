c8d6c40c1abb89d9ba1608045e7142b3fdfa88c4
ntut-xuan <t109590031@ntut.org.tw>
2022-10-24 09:42:48 +0000

    Fix pytest failed issues on sqlite3 schema and data

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,15 +1,9 @@
 CREATE TABLE IF NOT EXISTS `user` (
-  email TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  `uid` INT NOT NULL,
+  `email` TEXT NOT NULL,
+  `password` TEXT NOT NULL,
+  `firstname` TEXT NOT NULL,
+  `lastname` TEXT NOT NULL,
+  `sex` INT NOT NULL,
+  `birthday` INT NOT NULL
 )
-
-CREATE TABLE `user` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(400) NOT NULL,
-  `firstname` varchar(32) NOT NULL,
-  `lastname` varchar(32) NOT NULL,
-  `sex` tinyint(4) NOT NULL,
-  `birthday` int(11) NOT NULL,
-  PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
--- backend/tests/data.sql
+++ backend/tests/data.sql
@@ -1,11 +1,21 @@
-INSERT INTO `user` (email, password)
+INSERT INTO `user` (uid, email, password, firstname, lastname, sex, birthday)
 VALUES (
+    0,
     'test@email.com',
     -- test
-    'ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff'
+    'ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff',
+    'Huang',
+    'Han-Xuan',
+    0,
+    1666604387
   ),
   (
+    1,
     'other@email.com',
     -- other
-    '82a5cfd03cdcb713c8d7dfce41e6f0a92f6dc560e6dda56c11eb1a207aaa7689b07a1de30967fc040f8b0ef0672c1c2ad96fcacb95fb995f52ae5d657c094547'
+    '82a5cfd03cdcb713c8d7dfce41e6f0a92f6dc560e6dda56c11eb1a207aaa7689b07a1de30967fc040f8b0ef0672c1c2ad96fcacb95fb995f52ae5d657c094547',
+    'Uriah',
+    'Xuan',
+    0,
+    1666604387
   );
