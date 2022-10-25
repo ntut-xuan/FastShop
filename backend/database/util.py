85903a8d0603d8ff155b456f3752081fb58213f5
Lai-YT <381xvmvbib@gmail.com>
2022-10-25 15:57:15 +0000

    Add type hints & revise conditions

--- backend/database/util.py
+++ backend/database/util.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING, Any, cast
 
 import pymysql
 from flask import current_app, g
 
 if TYPE_CHECKING:
     from flask import Flask
+    from pymysql.cursors import Cursor
 
 
 def connect_database_for_app(app: Flask) -> None:
@@ -39,22 +40,28 @@ def connect_database() -> None:
     )
 
 
+# TODO: this method does more than one thing
 def execute_command(command: str, paramter: tuple) -> list:
-    conn = get_database()
-    cursor = conn.cursor()
+    # 1. execute command
+    conn: pymysql.Connection = get_database()
+    cursor: Cursor = conn.cursor()
     cursor.execute(command, paramter)
     conn.commit()
+
+    # 2. combine field with value to a dict
+    named_results: list[dict[str, Any]] = []
     if cursor.description != None:
-        field_name = [name[0] for name in cursor.description]
-        result = cursor.fetchall()
-        result_list = []
-        for data in result:
-            result_list.append(dict(zip(field_name, list(data))))
-        cursor.close()
-        return result_list
-    else:
-        cursor.close()
-        return []
+        # mysql may support better named attributes such as `with_rows` to check
+        # whether the executed operation could have produced rows,
+        # but we only use those defined in DB-API 2.0.
+
+        results: tuple[tuple] = cursor.fetchall()
+        field_names: list[str] = _get_field_names(cursor.description)
+        for data in results:
+            named_result: dict[str, Any] = dict(zip(field_names, list(data)))
+            named_results.append(named_result)
+    cursor.close()
+    return named_results
 
 
 def close_db(error: BaseException | None = None) -> None:
@@ -65,3 +72,11 @@ def close_db(error: BaseException | None = None) -> None:
 
 def _is_connected() -> bool:
     return "db" in g
+
+
+def _get_field_names(cursor_description: tuple[str, ...]) -> list[str]:
+    """
+    DB API returns a 7-tuple for each column where the last six items of each tuple are None,
+    which is useless.
+    """
+    return [name[0] for name in cursor_description]
