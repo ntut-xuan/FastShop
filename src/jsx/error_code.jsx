b104f074db450029cc1687af173266c6525b2b18
ntut-xuan <t109590031@ntut.org.tw>
2022-11-02 12:12:28 +0000

    Disuse the status code method

--- src/jsx/error_code.jsx
+++ src/jsx/error_code.jsx
@@ -1,20 +1,3 @@
-const ERROR_CODE = Object.freeze({
-    "INVALID_DATA": {"code": 301, "message": "未知的資料"},
-    "INVALID_EMAIL": {"code": 302, "message": "信箱格式錯誤"},
-    "INCORRECT_LOGIN": {"code": 401, "message": "登入失敗，帳號或密碼錯誤"}
-})
-
-function getMessageFromCode(code){
-    let keys = Object.keys(ERROR_CODE)
-    for(let i = 0; i < keys.length; i++){
-        let key = keys[i];
-        if(parseInt(ERROR_CODE[key]["code"]) === code){
-            console.log(code)
-            return ERROR_CODE[key]["message"]
-        }
-    }
-}
-
 function success_swal(title){
     return Swal.fire({
         icon: "success",
@@ -24,11 +7,11 @@ function success_swal(title){
     })
 }
 
-function error_swal(title, code){
+function error_swal(title, text){
     Swal.fire({
         icon: "error",
         title: title,
-        text: getMessageFromCode(code),
+        text: text,
         timer: 1500,
         showConfirmButton: false
     })
