2a9d5ac8471196a49ad3310eab0727b917ed5e63
Lai-YT <381xvmvbib@gmail.com>
2022-11-05 07:48:33 +0000

    Add integration test of same senario with unit test
    This is indeed from 67af06c682cf0a33978b97417250fb89b6d40fdf.

---
+++ backend/integration_tests/conftest.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Generator
+
+import pytest
+
+from app import create_app
+from database import create_database, get_database
+
+if TYPE_CHECKING:
+    from flask import Flask
+    from flask.testing import FlaskClient
+
+
+@pytest.fixture
+def app() -> Generator[Flask, None, None]:
+    app: Flask = create_app({"TESTING": False})
+
+    with app.app_context():
+        create_database()
+        with get_database().cursor() as cursor:
+            cursor.execute(
+                """
+                INSERT INTO `user` (`email`, `password`, `firstname`, `lastname`, `gender`, `birthday`)
+                VALUES (
+                    'test@email.com',
+                    -- test
+                    'ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff',
+                    'Han-Xuan',
+                    'Huang',
+                    0,
+                    1666604387  -- 2002-06-25
+                ),
+                (
+                    'other@email.com',
+                    -- other
+                    '82a5cfd03cdcb713c8d7dfce41e6f0a92f6dc560e6dda56c11eb1a207aaa7689b07a1de30967fc040f8b0ef0672c1c2ad96fcacb95fb995f52ae5d657c094547',
+                    'Xuan',
+                    'Uriah',
+                    0,
+                    1666604387  -- 2002-06-25
+                );
+                """
+            )
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS `test_table` (
+                    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+                    `account` TEXT NOT NULL UNIQUE,
+                    `password` TEXT NOT NULL
+                );
+                """
+            )
+            cursor.execute(
+                """
+                INSERT INTO `test_table` (`account`, `password`)
+                VALUES ('my_account', '#my_password'),
+                    ('other_account', '#other_password');
+                """
+            )
+        get_database().commit()
+
+    yield app
+
+    with (app.app_context(), get_database().cursor() as cursor):
+        cursor.execute("DROP TABLE IF EXISTS `test_table`;")
+        cursor.execute("DROP TABLE IF EXISTS `user`;")
+        cursor.execute("DROP TABLE IF EXISTS `new_table`;")
+        get_database().commit()
+
+
+@pytest.fixture
+def client(app: Flask) -> FlaskClient:
+    return app.test_client()
---
+++ backend/integration_tests/test_util.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import pytest
+from http import HTTPStatus
+from typing import TYPE_CHECKING
+
+from util import SingleMessageStatus, fetch_page
+
+if TYPE_CHECKING:
+    from flask.testing import FlaskClient
+    from werkzeug.test import TestResponse
+
+
+def test_fetch_page(client: FlaskClient) -> None:
+    with client.application.app_context():
+        page_content: str = fetch_page("index")
+
+    assert "index.html (a marker for API test)" in page_content
+
+
+def test_get_static_file_should_have_code_ok(client: FlaskClient) -> None:
+    response: TestResponse = client.get("/static/js/index.js")
+
+    assert response.status_code == HTTPStatus.OK
+    # not asserting data here because the file content may change
+
+
+class TestSingleMessageStatus:
+    def test_message_wrapped_as_dict(self) -> None:
+        status = SingleMessageStatus(HTTPStatus.OK, "page returned")
+
+        assert status.message == {"message": "page returned"}
+
+    @pytest.mark.parametrize(
+        argnames=("status_code",), argvalues=((102,), (226,), (308,))
+    )
+    def test_default_message_on_not_error_status_code_should_be_ok(
+        self, status_code: int
+    ) -> None:
+        status = SingleMessageStatus(status_code)
+
+        assert status.message["message"] == "OK"
+
+    @pytest.mark.parametrize(
+        argnames=("status_code",), argvalues=((400,), (451,), (500,), (511,))
+    )
+    def test_default_message_on_error_status_code_should_be_failed(
+        self, status_code: int
+    ) -> None:
+        status = SingleMessageStatus(status_code)
+
+        assert status.message["message"] == "Failed"
