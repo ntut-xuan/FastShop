584bf6e06361e1cea2119ddfcad95c5492b88ebe
Lai-YT <381xvmvbib@gmail.com>
2022-11-16 09:38:32 +0000

    Add String length for mysql & fix ArgumentError

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,12 +3,12 @@ from database import db
 
 class User(db.Model):
     uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String, nullable=False)
-    name = db.Column(db.String, nullable=False)
-    firstname = db.Column(db.String, nullable=False)
-    lastname = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
     gender = db.Column(db.Integer, nullable=False)
     birthday = db.Column(db.Integer, nullable=False)  # timestamp
 
-    __table_args__ = db.CheckConstraint(gender.in_({0, 1}))
+    __table_args__ = (db.CheckConstraint(gender.in_({0, 1})),)
