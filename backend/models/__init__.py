41354e0e480f0eedf8a8c298ab8a0de89860a0ec
Lai-YT <381xvmvbib@gmail.com>
2022-11-17 08:14:04 +0000

    Remove mis-placed column

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -5,7 +5,6 @@ class User(db.Model):  # type: ignore
     uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100), nullable=False)
-    name = db.Column(db.String(100), nullable=False)
     firstname = db.Column(db.String(100), nullable=False)
     lastname = db.Column(db.String(100), nullable=False)
     gender = db.Column(db.Integer, nullable=False)
