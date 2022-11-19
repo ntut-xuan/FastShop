a303148cb8bbe71c42b547859b573c5297f9c715
ntut-xuan <t109590031@ntut.org.tw>
2022-11-19 10:40:34 +0000

    Add response_message.py to provide the failed message

---
+++ backend/response_message.py
@@ -0,0 +1,4 @@
+DUPLICATED_ACCOUNT = "The email and password user post already exist."
+INCORRECT_EMAIL_OR_PASSWORD = "The email and password user post are not match."
+INVALID_DATA = "The data format user post is correct, but the data is invalid."
+WRONG_DATA_FORMAT = "The wrong format of data, the server can't understand the data."
