ed450a93a412f909d900e566780861f8e4f67ac2
ntut-xuan <t109590031@ntut.org.tw>
2022-12-01 01:21:45 +0000

    Reorder the function in app.py and adjust the name of function

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,31 @@ from database import create_db_command, db
 from util import fetch_page
 
 
-def init_swagger(app: Flask) -> None:
+def create_app(test_config: Mapping[str, Any] | None = None) -> Flask:
+    app = Flask(
+        __name__,
+        static_folder=(Path(__file__).parents[1] / "static"),
+    )
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.from_mapping(test_config)
+    # a random key for jwt encoding
+    app.config["jwt_key"] = token_hex()
+
+    _init_swagger(app)
+    db.init_app(app)
+    app.cli.add_command(create_db_command)
+    app.register_blueprint(auth_bp)
+
+    @app.route("/", methods=["GET"])
+    def index() -> str:
+        return fetch_page("index")
+
+    return app
+
+
+def _init_swagger(app: Flask) -> None:
     swagger_config = Swagger.DEFAULT_CONFIG
     swagger_config[
         "swagger_ui_bundle_js"
@@ -24,27 +48,3 @@ def init_swagger(app: Flask) -> None:
     ] = "//unpkg.com/swagger-ui-dist@4.15.5/swagger-ui.css"
 
     Swagger(app)
-
-
-def create_app(test_config: Mapping[str, Any] | None = None) -> Flask:
-    app = Flask(
-        __name__,
-        static_folder=(Path(__file__).parents[1] / "static"),
-    )
-    if test_config is None:
-        app.config.from_pyfile("config.py")
-    else:
-        app.config.from_mapping(test_config)
-    # a random key for jwt encoding
-    app.config["jwt_key"] = token_hex()
-
-    init_swagger(app)
-    db.init_app(app)
-    app.cli.add_command(create_db_command)
-    app.register_blueprint(auth_bp)
-
-    @app.route("/", methods=["GET"])
-    def index() -> str:
-        return fetch_page("index")
-
-    return app
