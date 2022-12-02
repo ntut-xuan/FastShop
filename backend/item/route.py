1085367fe7ce9077de171e45600ae053e408fb8c
ntut-xuan <t109590031@ntut.org.tw>
2022-12-02 16:41:35 +0000

    Finish route setup of GET, PUT, DELETE specific item

--- backend/item/route.py
+++ backend/item/route.py
@@ -16,3 +16,21 @@ def fetch_all_items():
 @swag_from("../api/items_post.yml")
 def add_item():
     pass
+
+
+@item_bp.route("/items/<string:ID>", methods=["GET"])
+@swag_from("../api/items_specific_item_get.yml")
+def fetch_specific_item(ID):
+    pass
+
+
+@item_bp.route("/items/<string:ID>", methods=["PUT"])
+@swag_from("../api/items_specific_item_put.yml")
+def update_specific_item(ID):
+    pass
+
+
+@item_bp.route("/items/<string:ID>", methods=["DELETE"])
+@swag_from("../api/items_specific_item_delete.yml")
+def delete_specific_item(ID):
+    pass
