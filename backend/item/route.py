47e430cae62f75662e35e4fd3c17052bccbf7814
ntut-xuan <t109590031@ntut.org.tw>
2022-12-02 09:30:32 +0000

    Complete items POST route description

--- backend/item/route.py
+++ backend/item/route.py
@@ -10,3 +10,9 @@ item_bp = Blueprint("item", __name__)
 @swag_from("../api/items_get.yml")
 def fetch_all_items():
     pass
+
+
+@item_bp.route("/items", methods=["POST"])
+@swag_from("../api/items_post.yml")
+def add_item():
+    pass
