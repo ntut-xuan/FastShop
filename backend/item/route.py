cb27952c86ea6a5a45c2ea49b102e1896d5ae336
ntut-xuan <t109590031@ntut.org.tw>
2022-12-03 11:26:40 +0000

    Ignore pass sentence coverage

--- backend/item/route.py
+++ backend/item/route.py
@@ -9,34 +9,34 @@ item_bp = Blueprint("item", __name__)
 @item_bp.route("/items", methods=["GET"])
 @swag_from("../api/items_get.yml")
 def fetch_all_items():
-    pass
+    pass  # coverage:ignore-line
 
 
 @item_bp.route("/items", methods=["POST"])
 @swag_from("../api/items_post.yml")
 def add_item():
-    pass
+    pass  # coverage:ignore-line
 
 
 @item_bp.route("/items/<string:ID>", methods=["GET"])
 @swag_from("../api/items_specific_item_get.yml")
 def fetch_specific_item(ID):
-    pass
+    pass  # coverage:ignore-line
 
 
 @item_bp.route("/items/<string:ID>", methods=["PUT"])
 @swag_from("../api/items_specific_item_put.yml")
 def update_specific_item(ID):
-    pass
+    pass  # coverage:ignore-line
 
 
 @item_bp.route("/items/<string:ID>", methods=["DELETE"])
 @swag_from("../api/items_specific_item_delete.yml")
 def delete_specific_item(ID):
-    pass
+    pass  # coverage:ignore-line
 
 
 @item_bp.route("/items/<string:ID>/count", methods=["GET"])
 @swag_from("../api/items_specific_item_count_get.yml")
 def fetch_count_of_specific_item(ID):
-    pass
+    pass  # coverage:ignore-line
