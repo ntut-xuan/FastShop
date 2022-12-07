f87b295248dc90cb1e4ff9647ed838a2d27ebb9f
ntut-xuan <t109590031@ntut.org.tw>
2022-12-07 02:30:42 +0000

    Adjust the path to get API documentation

--- backend/item/route.py
+++ backend/item/route.py
@@ -7,36 +7,36 @@ item_bp = Blueprint("item", __name__)
 
 
 @item_bp.route("/items", methods=["GET"])
-@swag_from("../api/item/items_get.yml")
+@swag_from("../api/item/get.yml")
 def fetch_all_items():
     pass  # pragma: no cover
 
 
 @item_bp.route("/items", methods=["POST"])
-@swag_from("../api/item/items_post.yml")
+@swag_from("../api/item/post.yml")
 def add_item():
     pass  # pragma: no cover
 
 
 @item_bp.route("/items/<string:id>", methods=["GET"])
-@swag_from("../api/item/items_id_get.yml")
+@swag_from("../api/item/id_get.yml")
 def fetch_specific_item(id):
     pass  # pragma: no cover
 
 
 @item_bp.route("/items/<string:id>", methods=["PUT"])
-@swag_from("../api/item/items_id_put.yml")
+@swag_from("../api/item/id_put.yml")
 def update_specific_item(id):
     pass  # pragma: no cover
 
 
 @item_bp.route("/items/<string:id>", methods=["DELETE"])
-@swag_from("../api/item/items_id_delete.yml")
+@swag_from("../api/item/id_delete.yml")
 def delete_specific_item(id):
     pass  # pragma: no cover
 
 
 @item_bp.route("/items/<string:id>/count", methods=["GET"])
-@swag_from("../api/item/items_id_count_get.yml")
+@swag_from("../api/item/id_count_get.yml")
 def fetch_count_of_specific_item(id):
     pass  # pragma: no cover
