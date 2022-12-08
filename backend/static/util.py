09fc8a10f00858ba684cfea78b512ef69bdc9109
Lai-YT <381xvmvbib@gmail.com>
2022-12-08 07:02:23 +0000

    Make function more explicit with verbose module prefix
    A single "exists" or "remove" does not show that it's an operation on path,
    so I import os instead of a specific function to keep the os. and os.path. prefix.

--- backend/static/util.py
+++ backend/static/util.py
@@ -1,22 +1,21 @@
-from os import remove
-from os.path import exists
+import os
 
 from flask import current_app
 
 from static.exception import FileNotExistError
 
 
-def has_image_with_specific_id (image_id: str) -> bool:
+def has_image_with_specific_id(image_id: str) -> bool:
     image_path: str = _get_file_path_by_image_id(image_id)
-    return exists(image_path)
+    return os.path.exists(image_path)
 
 
 def delete_image(image_id: str) -> None:
     image_path: str = _get_file_path_by_image_id(image_id)
-    if not has_image_with_specific_id (image_id):
+    if not has_image_with_specific_id(image_id):
         raise FileNotExistError(image_path)
     else:
-        remove(image_path)
+        os.remove(image_path)
 
 
 def write_static_image(image_data: str, image_id: str) -> None:
@@ -31,5 +30,5 @@ def _get_file_path_by_image_id(image_id: str) -> str:
 
 
 def _get_image_storage_path() -> str:
-    path: str = current_app.config.get("STATIC_RESOURCE_PATH") # type: ignore
+    path: str = current_app.config.get("STATIC_RESOURCE_PATH")  # type: ignore
     return path
