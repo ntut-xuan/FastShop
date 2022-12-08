717d6d3276003bfc1bc1d300d0cccc9d424941df
ntut-xuan <t109590031@ntut.org.tw>
2022-12-08 03:11:48 +0000

    Use fixture to make sure data is indenpendent, remove integration test

--- backend/tests/integration_tests/test_static_util.py
+++
@@ -1,5 +0,0 @@
-from tests.unit_tests.test_static_util import TestStorageImage
-
-
-class TestStorageImageWithProductionEnvironment(TestStorageImage):
-    pass
--- backend/tests/unit_tests/test_static_util.py
+++ backend/tests/unit_tests/test_static_util.py
@@ -14,18 +14,27 @@ from static.util import (
 if TYPE_CHECKING:
     from flask import Flask
 
-_some_image_base64_content = "data:image/png;base64,somecontent"
-_some_image_uuid = "c11d5bcf-f529-4318-904d-4bc8b8d7f68a"
+
+@pytest.fixture
+def image_object_fixture() -> tuple:
+    some_image_base64_content = "data:image/png;base64,somecontent"
+    some_image_uuid = "c11d5bcf-f529-4318-904d-4bc8b8d7f68a"
+    yield (some_image_base64_content, some_image_uuid)
 
 
 class TestStorageImage:
-    def test_put_image_by_absent_uuid_should_create_image(self, app: Flask) -> None:
+    def test_put_image_by_absent_uuid_should_create_image(
+        self, app: Flask, image_object_fixture: tuple
+    ) -> None:
+        content = image_object_fixture[0]
+        uuid = image_object_fixture[1]
+
         with app.app_context():
-            put_image(_some_image_base64_content, _some_image_uuid)  # Create image
+            put_image(content, uuid)  # Create image
 
-            assert is_image_with_specific_id_exist(_some_image_uuid)
+            assert is_image_with_specific_id_exist(uuid)
 
-            delete_image(_some_image_uuid)  # Teardown
+            delete_image(uuid)  # Teardown
 
     def test_delete_image_by_absent_uuid_should_throw_exceptions(
         self, app: Flask
@@ -34,9 +43,14 @@ class TestStorageImage:
             with app.app_context():
                 delete_image("a-b-c-d-eeeee")
 
-    def test_delete_image_by_exist_uuid_should_delete_image(self, app: Flask) -> None:
-        with app.app_context():
-            put_image(_some_image_base64_content, _some_image_uuid)  # Create image
-            delete_image(_some_image_uuid)  # Delete image
+    def test_delete_image_by_exist_uuid_should_delete_image(
+        self, app: Flask, image_object_fixture: tuple
+    ) -> None:
+        content = image_object_fixture[0]
+        uuid = image_object_fixture[1]
 
-            assert not is_image_with_specific_id_exist(_some_image_uuid)
+        with app.app_context():
+            put_image(content, uuid)  # Create image
+            delete_image(uuid)  # Delete image
+
+            assert not is_image_with_specific_id_exist(uuid)
