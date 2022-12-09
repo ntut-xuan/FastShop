985d0657cbfb215eb76589ac56e711a2b259b97d
Lai-YT <381xvmvbib@gmail.com>
2022-12-09 06:07:58 +0000

    Rarrange tag docs with hierarchy as same as tag route

--- backend/tag/route.py
+++ backend/tag/route.py
@@ -2,42 +2,36 @@ from __future__ import annotations
 
 from flask import Blueprint
 
-from util import register_swagger_file
+from util import route_with_doc
 
 tag_bp = Blueprint("tag", __name__)
 
 
-@tag_bp.route("/tags", methods=["GET"])
-@register_swagger_file("tag", "get", methods=["GET"])
+@route_with_doc(tag_bp, "/tags", methods=["GET"])
 def fetch_all_tags():
     pass  # pragma: no cover
 
 
-@tag_bp.route("/tags", methods=["POST"])
-@register_swagger_file("tag", "post", methods=["POST"])
+@route_with_doc(tag_bp, "/tags", methods=["POST"])
 def add_tag():
     pass  # pragma: no cover
 
 
-@tag_bp.route("/tags/<string:id>", methods=["GET"])
-@register_swagger_file("tag", "id_get", methods=["GET"])
+@route_with_doc(tag_bp, "/tags/<string:id>", methods=["GET"])
 def fetch_tag(id):
     pass  # pragma: no cover
 
 
-@tag_bp.route("/tags/<string:id>", methods=["PUT"])
-@register_swagger_file("tag", "id_put", methods=["PUT"])
+@route_with_doc(tag_bp, "/tags/<string:id>", methods=["PUT"])
 def update_tag(id):
     pass  # pragma: no cover
 
 
-@tag_bp.route("/tags/<string:id>", methods=["DELETE"])
-@register_swagger_file("tag", "id_delete", methods=["DELETE"])
+@route_with_doc(tag_bp, "/tags/<string:id>", methods=["DELETE"])
 def delete_tag(id):
     pass  # pragma: no cover
 
 
-@tag_bp.route("/tags/<string:id>/items", methods=["GET"])
-@register_swagger_file("tag", "id_items_get", methods=["GET"])
+@route_with_doc(tag_bp, "/tags/<string:id>/items", methods=["GET"])
 def get_items_by_tag(id):
     pass  # pragma: no cover
