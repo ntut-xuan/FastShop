fa68d603ced90c6cc1a70e6f24e6d3e89f773fce
Lai-YT <381xvmvbib@gmail.com>
2022-12-09 03:06:08 +0000

    Rarrange item docs with hierarchy as same as item route

--- backend/item/route.py
+++ backend/item/route.py
@@ -2,42 +2,36 @@ from __future__ import annotations
 
 from flask import Blueprint
 
-from util import register_swagger_file
+from util import route_with_doc
 
 item_bp = Blueprint("item", __name__)
 
 
-@item_bp.route("/items", methods=["GET"])
-@register_swagger_file("item", "get", methods=["GET"])
+@route_with_doc(item_bp, "/items", methods=["GET"])
 def fetch_all_items():
     pass  # pragma: no cover
 
 
-@item_bp.route("/items", methods=["POST"])
-@register_swagger_file("item", "post", methods=["POST"])
+@route_with_doc(item_bp, "/items", methods=["POST"])
 def add_item():
     pass  # pragma: no cover
 
 
-@item_bp.route("/items/<string:id>", methods=["GET"])
-@register_swagger_file("item", "id_get", methods=["GET"])
+@route_with_doc(item_bp, "/items/<string:id>", methods=["GET"])
 def fetch_specific_item(id):
     pass  # pragma: no cover
 
 
-@item_bp.route("/items/<string:id>", methods=["PUT"])
-@register_swagger_file("item", "id_put", methods=["PUT"])
+@route_with_doc(item_bp, "/items/<string:id>", methods=["PUT"])
 def update_specific_item(id):
     pass  # pragma: no cover
 
 
-@item_bp.route("/items/<string:id>", methods=["DELETE"])
-@register_swagger_file("item", "id_delete", methods=["DELETE"])
+@route_with_doc(item_bp, "/items/<string:id>", methods=["DELETE"])
 def delete_specific_item(id):
     pass  # pragma: no cover
 
 
-@item_bp.route("/items/<string:id>/count", methods=["GET"])
-@register_swagger_file("item", "id_count_get", methods=["GET"])
+@route_with_doc(item_bp, "/items/<string:id>/count", methods=["GET"])
 def fetch_count_of_specific_item(id):
     pass  # pragma: no cover
