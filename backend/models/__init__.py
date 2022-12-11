0007b495a36c7e29e7512f32eea675b128243fff
Lai-YT <381xvmvbib@gmail.com>
2022-12-11 14:50:38 +0000

    Define table of item

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,5 +1,7 @@
 from database import db
 
+from sqlalchemy.engine.default import DefaultExecutionContext
+
 
 class User(db.Model):  # type: ignore
     uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -16,3 +18,23 @@ class User(db.Model):  # type: ignore
 class Tag(db.Model):  # type: ignore
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
+
+
+def same_as(column_name: str):
+    def default_function(context: DefaultExecutionContext):
+        return context.current_parameters.get(column_name)
+
+    return default_function
+
+
+class Item(db.Model):  # type: ignore
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    count = db.Column(db.Integer, default=0, nullable=False)
+    original = db.Column(db.Integer, nullable=False)
+    discount = db.Column(
+        db.Integer, default=same_as("original"), nullable=False
+    )  # price after discounted
+    avatar = db.Column(db.String(36))  # 36 character long uuid
+
+    __table_args__ = (db.CheckConstraint(count > 0),)
