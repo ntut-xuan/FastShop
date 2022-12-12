5d61a00f2c3cba5050fdf24951edc181ea4f27d2
Lai-YT <381xvmvbib@gmail.com>
2022-12-12 13:51:04 +0000

    Fix cascade not working
    This is because SQLite does not support cascading by default.

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
+import sqlite3
 from typing import Final
 
 import click
 from flask import current_app
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 
 
 db: Final[SQLAlchemy] = SQLAlchemy()
@@ -12,6 +15,17 @@ db: Final[SQLAlchemy] = SQLAlchemy()
 # For SQLAlchemy.create_all to know what to create.
 # NOTE: imports after the creation of "db" to resolve circular import
 from models import User
+
+
+# SQLite does not work with foreign key by default.
+# The PRAGMA for enablement must be emitted on all connections before use.
+# See https://docs.sqlalchemy.org/en/20/dialects/sqlite.html#foreign-key-support
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, _):
+    if isinstance(dbapi_connection, sqlite3.Connection):
+        cursor: sqlite3.Cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
 
 
 @click.command("create-db")
