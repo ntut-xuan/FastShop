fe458896c0914fe3367f3eb6fc1ebdf02f0a0d35
ntut-xuan <t109590031@ntut.org.tw>
2022-12-14 06:16:18 +0000

    Use pathlib to instead string path

--- backend/static/util.py
+++ backend/static/util.py
@@ -9,35 +9,35 @@ from static.exception import ImageNotExistError
 
 
 def has_image_with_specific_id(image_id: str) -> bool:
-    image_path: str = get_file_path_by_image_uuid(image_id)
-    return os.path.exists(image_path)
+    image_path: Path = get_file_path_by_image_uuid(image_id)
+    return image_path.exists()
 
 
 def delete_image(image_id: str) -> None:
-    image_path: str = get_file_path_by_image_uuid(image_id)
+    image_path: Path = get_file_path_by_image_uuid(image_id)
     try:
-        os.remove(image_path)
+        image_path.unlink()
     except FileNotFoundError:
         raise ImageNotExistError(image_path)
 
 
 def write_image_with_byte_data(byte_data: bytes, image_uuid: str) -> None:
-    image_path: str = get_file_path_by_image_uuid(image_uuid)
-    Path(image_path).write_bytes(byte_data)
+    image_path: Path = get_file_path_by_image_uuid(image_uuid)
+    image_path.write_bytes(byte_data)
 
 
 def get_image_byte_from_existing_file(image_uuid: str) -> bytes:
-    image_path: str = get_file_path_by_image_uuid(image_uuid)
-    return Path(image_path).read_bytes()
+    image_path: Path = get_file_path_by_image_uuid(image_uuid)
+    return image_path.read_bytes()
 
 
-def get_file_path_by_image_uuid(uuid: str) -> str:
+def get_file_path_by_image_uuid(uuid: str) -> Path:
     """
     Image with its id as `image_id` has path `STATIC_RESOURCE_PATH`/`image_id`.png,
     where `STATIC_RESOURCE_PATH` is configured in config.py.
     """
     static_resource_path: str = current_app.config.get("STATIC_RESOURCE_PATH")  # type: ignore
-    return f"{static_resource_path}/{uuid}.png"
+    return Path(f"{static_resource_path}/{uuid}.png")
 
 
 def get_image_byte_data_from_base64_content(base64_content: str):
