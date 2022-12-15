fb4662eae4b4fe9df442e9bc5c4e43fa87705bed
ntut-xuan <t109590031@ntut.org.tw>
2022-12-15 01:07:21 +0000

    Add ABSENT_IMAGE_WITH_SPECIFIC_UUID and INVALID_UUID two message

--- backend/response_message.py
+++ backend/response_message.py
@@ -1,8 +1,10 @@
 ABSENT_COOKIE = "The specific cookie does not exist in the request header."
+ABSENT_IMAGE_WITH_SPECIFIC_UUID = "The image with a specific UUID is absent."
 DUPLICATE_ACCOUNT = "The posted email already exist."
 INCORRECT_EMAIL_OR_PASSWORD = (
     "The email or password that the user posted does not match any account."
 )
 INVALID_COOKIE = "The specific cookie in the request header is invalid."
 INVALID_DATA = "The posted data has the correct format, but the data is invalid."
+INVALID_UUID = "The specific UUID in the path parameter is invalid."
 WRONG_DATA_FORMAT = "The data has the wrong format and the server can't understand it."
