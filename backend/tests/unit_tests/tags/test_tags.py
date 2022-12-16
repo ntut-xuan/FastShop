73618e6d5e12e8d01d2cfde29a34fe0f39a57a55
Lai-YT <381xvmvbib@gmail.com>
2022-12-16 08:47:07 +0000

    Insert test tags with test fixture

--- backend/tests/unit_tests/tags/test_tags.py
+++ backend/tests/unit_tests/tags/test_tags.py
@@ -21,10 +21,27 @@ def logged_in_client(client: FlaskClient) -> FlaskClient:
     return client
 
 
+@pytest.fixture
+def test_tags(app: Flask) -> list[dict[str, Any]]:
+    """Inserts some tags into the database.
+
+    Returns:
+       A list of dicts contains the attributes of the inserted tags.
+    """
+    tags: list[dict[str, Any]] = [
+        {"id": 1, "name": "seafood"},
+        {"id": 2, "name": "fruit"},
+        {"id": 3, "name": "solid food"},
+    ]
+    with app.app_context():
+        db.session.execute(db.insert(Tag), tags)
+        db.session.commit()
+    return tags
+
+
 class TestGetTagsRoute:
     def test_should_return_count_0_if_no_existing_tags(
-        self,
-        client: FlaskClient,
+        self, client: FlaskClient
     ) -> None:
         response: TestResponse = client.get("/tags")
 
@@ -33,26 +50,16 @@ class TestGetTagsRoute:
         assert count == 0
 
     def test_should_return_all_existing_tags(
-        self, app: Flask, client: FlaskClient
+        self, client: FlaskClient, test_tags: list[dict[str, Any]]
     ) -> None:
-        tags: list[dict[str, Any]] = [
-            {"id": 1, "name": "seafood"},
-            {"id": 2, "name": "fruit"},
-            {"id": 3, "name": "solid food"},
-        ]
-        with app.app_context():
-            db.session.execute(db.insert(Tag), tags)
-            db.session.commit()
-
         response: TestResponse = client.get("/tags")
 
         assert response.json is not None
         data: dict[str, Any] = response.json
-        assert data["count"] == 3
+        assert data["count"] == len(test_tags)
         responded_tags: list[dict[str, Any]] = data["tags"]
-        assert responded_tags[0] in tags
-        assert responded_tags[1] in tags
-        assert responded_tags[2] in tags
+        for tag in responded_tags:
+            assert tag in test_tags
 
 
 class PostTagsRoute:
@@ -95,12 +102,9 @@ class PostTagsRoute:
         assert response.get_json(silent=True) == {"message": "Unauthorized."}
 
     def test_with_existing_tag_should_respond_forbidden_with_message(
-        self, app: Flask, logged_in_client: FlaskClient
+        self, logged_in_client: FlaskClient, test_tags: list[dict[str, Any]]
     ) -> None:
-        existing_tag: dict[str, Any] = {"id": 1, "name": "black magic"}
-        with app.app_context():
-            db.session.add(Tag(**existing_tag))
-            db.session.commit()
+        existing_tag: dict[str, Any] = test_tags[0]
 
         response: TestResponse = logged_in_client.post("/tags", json=existing_tag)
 
@@ -124,13 +128,9 @@ class PostTagsRoute:
 
 class TestGetTagsByIdRoute:
     def test_should_return_the_tag_if_exist(
-        self, app: Flask, client: FlaskClient
+        self, client: FlaskClient, test_tags: list[dict[str, Any]]
     ) -> None:
-        existing_tag: dict[str, Any] = {"id": 1, "name": "black magic"}
-        with app.app_context():
-            db.session.add(Tag(**existing_tag))
-            db.session.add(Tag(id=2, name="some other tag"))
-            db.session.commit()
+        existing_tag: dict[str, Any] = test_tags[0]
 
         response: TestResponse = client.get(f"/tags/{existing_tag['id']}")
 
@@ -152,13 +152,9 @@ class TestGetTagsByIdRoute:
 
 class TestDeleteTagsByIdRoute:
     def test_should_response_ok_with_message_if_exist(
-        self, app: Flask, logged_in_client: FlaskClient
+        self, logged_in_client: FlaskClient, test_tags: list[dict[str, Any]]
     ) -> None:
-        existing_tag: dict[str, Any] = {"id": 1, "name": "black magic"}
-        with app.app_context():
-            db.session.add(Tag(**existing_tag))
-            db.session.add(Tag(id=2, name="some other tag"))
-            db.session.commit()
+        existing_tag: dict[str, Any] = test_tags[0]
 
         response: TestResponse = logged_in_client.delete(f"/tags/{existing_tag['id']}")
 
@@ -166,13 +162,9 @@ class TestDeleteTagsByIdRoute:
         assert response.get_json(silent=True) == {"message": "OK"}
 
     def test_should_delete_the_tag_if_exist(
-        self, app: Flask, logged_in_client: FlaskClient
+        self, app: Flask, logged_in_client: FlaskClient, test_tags: list[dict[str, Any]]
     ) -> None:
-        target: dict[str, Any] = {"id": 1, "name": "black magic"}
-        with app.app_context():
-            db.session.add(Tag(**target))
-            db.session.add(Tag(id=2, name="some other tag"))
-            db.session.commit()
+        target: dict[str, Any] = test_tags[0]
 
         logged_in_client.delete(f"/tags/{target['id']}")
 
@@ -183,12 +175,9 @@ class TestDeleteTagsByIdRoute:
             assert tag is None
 
     def test_without_logging_in_should_respond_unauthorized_with_message(
-        self, app: Flask, client: FlaskClient
+        self, client: FlaskClient, test_tags: list[dict[str, Any]]
     ) -> None:
-        existing_tag: dict[str, Any] = {"id": 1, "name": "black magic"}
-        with app.app_context():
-            db.session.add(Tag(**existing_tag))
-            db.session.commit()
+        existing_tag: dict[str, Any] = test_tags[0]
 
         response: TestResponse = client.delete(f"/tags/{existing_tag['id']}")
 
