35fc2a689079a22f539c43cdb6ffbef7760ec897
ntut-xuan <t109590031@ntut.org.tw>
2022-12-16 09:02:56 +0000

    Rename function, add convert tags dict list function

--- backend/item/util.py
+++ backend/item/util.py
@@ -29,18 +29,24 @@ class ItemData:
     tags: list[TagData] = Field([])
 
 
-def get_item_object_from_item_data_dict(dict: dict[str, Any]):
+def covert_item_object(item_data_dict: dict[str, Any]):
     item = ItemData(
-        avatar=dict["avatar"],
-        count=dict["count"],
-        name=dict["name"],
-        price=PriceData(original=dict["price"]["original"]),
+        avatar=item_data_dict["avatar"],
+        count=item_data_dict["count"],
+        name=item_data_dict["name"],
+        price=PriceData(original=item_data_dict["price"]["original"]),
     )
-    if "discount" in dict["price"]:
-        item.price.discount = dict["price"]["discount"]
-    if "tags" in dict:
-        item.tags = [
-            TagData(id=tags_data["id"], name=tags_data["name"])
-            for tags_data in dict["tags"]
-        ]
+    if "discount" in item_data_dict["price"]:
+        item.price.discount = item_data_dict["price"]["discount"]
+    if "tags" in item_data_dict:
+        item.tags = convert_tags_object_list(item_data_dict["tags"])
     return item
+
+
+def convert_tags_object_list(tags_dict_list: list[dict[str, Any]]):
+    tags_object_list = []
+
+    for tags_data in tags_dict_list:
+        tags_object_list.append(TagData(id=tags_data["id"], name=tags_data["name"]))
+
+    return tags_object_list
--- backend/tests/unit_tests/item/test_util.py
+++ backend/tests/unit_tests/item/test_util.py
@@ -5,24 +5,25 @@ from typing import Any
 import pytest
 from pydantic.error_wrappers import ValidationError
 
-from item.util import ItemData, get_item_object_from_item_data_dict
+from item.util import ItemData, TagData, covert_item_object, convert_tags_object_list
+
+
+@pytest.fixture
+def item_data_dict() -> dict:
+    return {
+        "avatar": "f692073a-7ac1-11ed-a1eb-0242ac120002",
+        "count": 44,
+        "name": "Entropy",
+        "price": {"discount": 43210, "original": 48763},
+        "tags": [{"id": 33, "name": "dian"}],
+    }
 
 
 class TestGetItemFromItemDataDict:
-    @pytest.fixture
-    def item_data_dict(self) -> dict:
-        return {
-            "avatar": "f692073a-7ac1-11ed-a1eb-0242ac120002",
-            "count": 44,
-            "name": "Entropy",
-            "price": {"discount": 43210, "original": 48763},
-            "tags": [{"id": 33, "name": "dian"}],
-        }
-
     def test_valid_dict_should_return_correct_item_object(
         self, item_data_dict: dict[str, Any]
     ):
-        item: ItemData = get_item_object_from_item_data_dict(item_data_dict)
+        item: ItemData = covert_item_object(item_data_dict)
 
         assert item.avatar == "f692073a-7ac1-11ed-a1eb-0242ac120002"
         assert item.count == 44
@@ -34,7 +35,7 @@ class TestGetItemFromItemDataDict:
 
     def test_incorrect_format_dict_should_raise_missing_fields_error(self):
         with pytest.raises(KeyError):
-            get_item_object_from_item_data_dict({"wrong_column": "wrong_value"})
+            covert_item_object({"wrong_column": "wrong_value"})
 
     def test_invalid_data_dict_should_raise_error(self, item_data_dict: dict[str, Any]):
         item_data_dict[
@@ -42,14 +43,24 @@ class TestGetItemFromItemDataDict:
         ] = "5"  # Since count require integer-type data, it should cause error.
 
         with pytest.raises(ValidationError):
-            get_item_object_from_item_data_dict(item_data_dict)
+            covert_item_object(item_data_dict)
 
     def test_no_tags_dict_should_ok(self, item_data_dict: dict[str, Any]):
         del item_data_dict["tags"]
 
-        get_item_object_from_item_data_dict(item_data_dict)
+        covert_item_object(item_data_dict)
 
     def test_no_discount_price_dict_should_ok(self, item_data_dict: dict[str, Any]):
         del item_data_dict["price"]["discount"]
 
-        get_item_object_from_item_data_dict(item_data_dict)
+        covert_item_object(item_data_dict)
+
+
+def test_tags_dict_list_convert_to_tags_object_list_should_ok(
+    item_data_dict: dict[str, Any]
+):
+    tags_object_list = convert_tags_object_list(item_data_dict["tags"])
+
+    assert isinstance(tags_object_list, list)
+    for object in tags_object_list:
+        assert isinstance(object, TagData)
