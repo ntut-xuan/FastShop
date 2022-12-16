3976e4c81c67002011b550784384d051118237b8
ntut-xuan <t109590031@ntut.org.tw>
2022-12-16 11:08:02 +0000

    Complete add item util & remove discount optional

--- backend/item/util.py
+++ backend/item/util.py
@@ -1,9 +1,16 @@
 from typing import TYPE_CHECKING, Any
 
 from pydantic import Field, StrictInt, StrictStr
+from sqlalchemy import func
+
+from database import db
+from models import Item
+
 
 if TYPE_CHECKING:
     from dataclasses import dataclass
+    from sqlalchemy.sql.selectable import Select
+    from sqlalchemy.sql.dml import Insert, Update
 else:
     from pydantic.dataclasses import dataclass
 
@@ -36,8 +43,6 @@ def covert_item_object(item_data_dict: dict[str, Any]):
         name=item_data_dict["name"],
         price=PriceData(original=item_data_dict["price"]["original"]),
     )
-    if "discount" in item_data_dict["price"]:
-        item.price.discount = item_data_dict["price"]["discount"]
     if "tags" in item_data_dict:
         item.tags = convert_tags_object_list(item_data_dict["tags"])
     return item
@@ -50,3 +55,19 @@ def convert_tags_object_list(tags_dict_list: list[dict[str, Any]]):
         tags_object_list.append(TagData(id=tags_data["id"], name=tags_data["name"]))
 
     return tags_object_list
+
+
+def add_item_data(data: ItemData) -> int:
+    insert_data_stmts: Insert = db.insert(Item).values(
+        avatar=data.avatar,
+        count=data.count,
+        discount=data.price.discount,
+        name=data.name,
+        original=data.price.original,
+    )
+    db.session.execute(insert_data_stmts)
+    db.session.commit()
+    
+    select_data_count_stmts: Select = db.select([func.count(Item.id)]).select_from(Item)
+    id = db.session.execute(select_data_count_stmts).scalar_one()
+    return id
--- backend/tests/unit_tests/item/test_util.py
+++ backend/tests/unit_tests/item/test_util.py
@@ -1,11 +1,18 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, TYPE_CHECKING
 
 import pytest
 from pydantic.error_wrappers import ValidationError
+from sqlalchemy import func
 
-from item.util import ItemData, TagData, covert_item_object, convert_tags_object_list
+from database import db
+from item.util import ItemData, TagData, add_item_data, covert_item_object, convert_tags_object_list
+from models import Item
+
+if TYPE_CHECKING:
+    from flask import Flask
+    from sqlalchemy.sql.selectable import Select
 
 
 @pytest.fixture
@@ -17,6 +24,20 @@ def item_data_dict() -> dict:
         "price": {"discount": 43210, "original": 48763},
         "tags": [{"id": 33, "name": "dian"}],
     }
+
+def is_item_tuple_and_item_data_object_equals(item: tuple[Any], item_data: ItemData) -> bool:
+    return (
+        item[1] == item_data.name and # Column 2 is name
+        item[2] == item_data.count and # Column 3 is count
+        item[3] == item_data.price.original and # Column 4 is original price
+        item[4] == item_data.price.discount and # Column 5 is discount price
+        item[5] == item_data.avatar # Column 6 is avatar
+    )
+
+
+@pytest.fixture
+def item_data(item_data_dict: dict[str, Any]) -> ItemData:
+    return covert_item_object(item_data_dict)
 
 
 class TestCovertItemDataObjectFromItemDataDict:
@@ -64,3 +85,17 @@ def test_tags_dict_list_convert_to_tags_object_list_should_ok(
     assert isinstance(tags_object_list, list)
     for object in tags_object_list:
         assert isinstance(object, TagData)
+
+
+def test_add_item_by_id_should_ok(app: Flask, item_data: ItemData):
+    count_item_stmt: Select = db.select([func.count(Item.id)]).select_from(Item).where(Item.id == id)
+    item_data_select_stmt: Select = db.select(['*']).select_from(Item).where(Item.id == id)
+    with app.app_context():
+    
+        id = add_item_data(item_data)
+        count: int = db.session.execute(count_item_stmt).scalar_one()
+        item: tuple = db.session.execute(item_data_select_stmt).one()
+        
+        assert count == 1
+        assert is_item_tuple_and_item_data_object_equals(item, item_data)
+        
