646a74e299762f0e8223b5ecb871a67ca6c850af
Lai-YT <381xvmvbib@gmail.com>
2022-12-21 15:51:05 +0000

    Add integration tests of items route

---
+++ backend/tests/integration_tests/test_item.py
@@ -0,0 +1,36 @@
+from tests.unit_tests.item.test_item import (
+    TestDeleteItemsByIdRoute,
+    TestGetItemsByIdRoute,
+    TestGetItemsCountByIdRoute,
+    TestGetItemsRoute,
+    TestPostItemsRoute,
+    TestPutItemsByIdRoute,
+    # test fixtures
+    build_tags,
+    logged_in_client,
+    setup_item,
+)
+
+
+class TestDeleteItemsByIdRouteWithMariaDB(TestDeleteItemsByIdRoute):
+    pass
+
+
+class TestGetItemsByIdRouteWithMariaDB(TestGetItemsByIdRoute):
+    pass
+
+
+class TestGetItemsCountByIdRouteWithTestGetItemsRoute(TestGetItemsCountByIdRoute):
+    pass
+
+
+class TestGetItemsRouteTestGetItemsRoute(TestGetItemsRoute):
+    pass
+
+
+class TestPostItemsRouteWithMariaDB(TestPostItemsRoute):
+    pass
+
+
+class TestPutItemsByIdRouteWithMariaDB(TestPutItemsByIdRoute):
+    pass
