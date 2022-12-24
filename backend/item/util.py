b6600f2e12cb875fc480e7fb6e7db90c6c0d14d3
ntut-xuan <t109590031@ntut.org.tw>
2022-12-24 09:46:44 +0000

    Finish the fix of testcase and route

--- backend/item/util.py
+++ backend/item/util.py
@@ -9,6 +9,7 @@ class PayloadTypeChecker:
     class Item:
         avatar: StrictStr = Field(default="")
         count: StrictInt = Field(default=0)
+        description: StrictStr = Field(default="")
         name: StrictStr = Field(default="")
         original: StrictInt = Field(default=0)
         discount: StrictInt = Field(default=0)
@@ -24,6 +25,7 @@ def flatten_item_payload(payload: dict[str, Any]) -> dict[str, Any]:
     flat_payload: dict[str, Any] = {
         "avatar": payload["avatar"],
         "count": payload["count"],
+        "description": payload["description"],
         "name": payload["name"],
         "original": payload["price"]["original"],
         "discount": payload["price"]["discount"],
