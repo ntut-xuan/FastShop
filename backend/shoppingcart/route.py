082f186421de91b8f22772e14c7cea99155ef75e
ntut-xuan <t109590031@ntut.org.tw>
2022-12-28 17:27:10 +0000

    Add pragma: no cover in route

--- backend/shoppingcart/route.py
+++ backend/shoppingcart/route.py
@@ -7,19 +7,19 @@ shoppingcart_bp = Blueprint("shoppingcart", __name__)
 
 @route_with_doc(shoppingcart_bp, "/shoppingcart", methods=["GET"])
 def get_the_shoppingcart():
-    pass
+    pass  # pragma: no cover.
 
 
 @route_with_doc(shoppingcart_bp, "/shoppingcart/item", methods=["POST"])
 def add_one_item_to_the_shoppingcart():
-    pass
+    pass  # pragma: no cover.
 
 
 @route_with_doc(shoppingcart_bp, "/shoppingcart/item", methods=["PUT"])
 def update_one_item_to_the_shoppingcart():
-    pass
+    pass  # pragma: no cover.
 
 
 @route_with_doc(shoppingcart_bp, "/shoppingcart", methods=["DELETE"])
 def delete_the_shoppingcart():
-    pass
+    pass  # pragma: no cover.
