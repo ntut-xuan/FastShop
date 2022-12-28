1318c13b945d365890a649cb98424606ef71ef1a
ntut-xuan <t109590031@ntut.org.tw>
2022-12-28 17:20:27 +0000

    Complete the shopping cart api documentation

---
+++ backend/shoppingcart/route.py
@@ -0,0 +1,25 @@
+from flask import Blueprint
+
+from util import route_with_doc
+
+shoppingcart_bp = Blueprint("shoppingcart", __name__)
+
+
+@route_with_doc(shoppingcart_bp, "/shoppingcart", methods=["GET"])
+def get_the_shoppingcart():
+    pass
+
+
+@route_with_doc(shoppingcart_bp, "/shoppingcart/item", methods=["POST"])
+def add_one_item_to_the_shoppingcart():
+    pass
+
+
+@route_with_doc(shoppingcart_bp, "/shoppingcart/item", methods=["PUT"])
+def update_one_item_to_the_shoppingcart():
+    pass
+
+
+@route_with_doc(shoppingcart_bp, "/shoppingcart", methods=["DELETE"])
+def delete_the_shoppingcart():
+    pass
