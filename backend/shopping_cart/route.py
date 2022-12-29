98c4d148d3f605246c63539d939b0c54b08163a7
ntut-xuan <t109590031@ntut.org.tw>
2022-12-29 03:50:52 +0000

    Rename function and tags

--- backend/shopping_cart/route.py
+++ backend/shopping_cart/route.py
@@ -6,20 +6,20 @@ shopping_cart_bp = Blueprint("shopping_cart", __name__)
 
 
 @route_with_doc(shopping_cart_bp, "/shopping_cart", methods=["GET"])
-def get_the_shoppingcart():
+def get_the_shopping_cart():
     pass  # pragma: no cover.
 
 
 @route_with_doc(shopping_cart_bp, "/shopping_cart/item", methods=["POST"])
-def add_one_item_to_the_shoppingcart():
+def add_one_item_to_the_shopping_cart():
     pass  # pragma: no cover.
 
 
 @route_with_doc(shopping_cart_bp, "/shopping_cart/item", methods=["PUT"])
-def update_one_item_to_the_shoppingcart():
+def update_one_item_to_the_shopping_cart():
     pass  # pragma: no cover.
 
 
 @route_with_doc(shopping_cart_bp, "/shopping_cart", methods=["DELETE"])
-def delete_the_shoppingcart():
+def delete_the_shopping_cart():
     pass  # pragma: no cover.
