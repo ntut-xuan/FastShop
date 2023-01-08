d8e432bd213dbd47f2765a24122a82a2fe762f90
ntut-xuan <t109590031@ntut.org.tw>
2023-01-08 19:46:26 +0000

    Finish GET /shoppingcart

--- backend/shopping_cart/route.py
+++ backend/shopping_cart/route.py
@@ -1,11 +1,14 @@
-from typing import Any, cast
+from typing import TYPE_CHECKING, Any, cast
 
-from flask import Blueprint, current_app
+from flask import Blueprint, current_app, make_response, request
 
-from auth.util import HS256JWTCodec
+from auth.util import HS256JWTCodec, verify_login_or_return_401
 from database import db
-from models import User
+from models import Item, ShoppingCart, User
 from util import route_with_doc
+
+if TYPE_CHECKING:
+    from sqlalchemy.engine.row import Row
 
 shopping_cart_bp = Blueprint("shopping_cart", __name__)
 
@@ -20,8 +23,34 @@ def fetch_user_id_from_jwt_token(jwt_token: str) -> int:
 
 
 @route_with_doc(shopping_cart_bp, "/shopping_cart", methods=["GET"])
+@verify_login_or_return_401
 def get_the_shopping_cart():
-    pass  # pragma: no cover.
+    jwt_token: str = request.cookies.get("jwt")
+    user_id: int = fetch_user_id_from_jwt_token(jwt_token)
+
+    # Item detail should have ID, count and discount.
+    user_cart_item_details: list[Row] = db.session.execute(
+        db.select(ShoppingCart.item_id, ShoppingCart.count, Item.discount)
+        .select_from(ShoppingCart)
+        .join(Item)
+        .where(ShoppingCart.user_id == user_id)
+    ).all()
+
+    items: list[dict[str, Any]] = []
+    items_total_price = 0
+
+    for item_details in user_cart_item_details:
+        items.append(
+            {
+                "count": item_details.count,
+                "id": item_details.item_id,
+                "price": item_details.discount,
+            }
+        )
+        items_total_price += item_details.discount * item_details.count
+
+    result = {"count": len(items), "items": items, "price": items_total_price}
+    return make_response(result)
 
 
 @route_with_doc(shopping_cart_bp, "/shopping_cart/item", methods=["POST"])
--- backend/tests/unit_tests/shopping_cart/test_shopping_cart.py
+++ backend/tests/unit_tests/shopping_cart/test_shopping_cart.py
@@ -1,15 +1,73 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from http import HTTPStatus
+from typing import TYPE_CHECKING, Any
 
 import pytest
 
+from database import db
+from models import Item, ShoppingCart, Tag, TagOfItem
 from shopping_cart.route import fetch_user_id_from_jwt_token
 
 if TYPE_CHECKING:
     from http.cookiejar import Cookie, CookieJar
     from flask import Flask
     from flask.testing import FlaskClient
+    from werkzeug.test import TestResponse
+
+
+@pytest.fixture
+def setup_item(app: Flask) -> None:
+    with app.app_context():
+        db.session.execute(
+            db.insert(Item),
+            [
+                {
+                    "id": 1,
+                    "name": "apple",
+                    "count": 10,
+                    "description": "This is an apple.",
+                    "original": 30,
+                    "discount": 25,
+                    "avatar": "xx-S0m3-aVA7aR-0f-a991e-xx",
+                },
+                {
+                    "id": 2,
+                    "name": "tilapia",
+                    "count": 3,
+                    "description": "This is a tilapia.",
+                    "original": 50,
+                    "discount": 45,
+                    "avatar": "xx-S0m3-aVA7aR-0f-ti1a9iA-xx",
+                },
+            ],
+        )
+        db.session.execute(
+            db.insert(Tag),
+            [
+                {"id": 1, "name": "fruit"},
+                {"id": 2, "name": "fish"},
+                {"id": 3, "name": "grocery"},
+            ],
+        )
+        db.session.execute(
+            db.insert(TagOfItem),
+            [
+                {"item_id": 1, "tag_id": 1},
+                {"item_id": 2, "tag_id": 2},
+                {"item_id": 1, "tag_id": 3},
+                {"item_id": 2, "tag_id": 3},
+            ],
+        )
+        db.session.execute(
+            db.insert(ShoppingCart),
+            [
+                {"user_id": 1, "item_id": 1, "count": 5},
+                {"user_id": 1, "item_id": 2, "count": 8},
+                {"user_id": 2, "item_id": 2, "count": 7},
+            ],
+        )
+        db.session.commit()
 
 
 def test_user_id_fetch_function_with_logined_client_should_return_user_id(
@@ -24,6 +82,43 @@ def test_user_id_fetch_function_with_logined_client_should_return_user_id(
         user_id: int = fetch_user_id_from_jwt_token(jwt_token)
 
         assert user_id == 1  # The user with test@mail.com UID is 1.
+
+
+class TestGetShoppingCart:
+    def test_with_logged_in_client_should_respond_excepted_response(
+        self, app: Flask, logged_in_client: FlaskClient, setup_item: None
+    ):
+        excepted_response = {
+            "count": 2,
+            "items": [
+                {
+                    "count": 5,
+                    "id": 1,
+                    "price": 25,
+                },
+                {
+                    "count": 8,
+                    "id": 2,
+                    "price": 45,
+                },
+            ],
+            "price": 485,  # 5*25 + 8*45 = 485
+        }
+        with app.app_context():
+
+            response: TestResponse = logged_in_client.get("shopping_cart")
+
+            response_payload: dict[str, Any] | None = response.json
+            assert response_payload == excepted_response
+
+    def test_with_not_logged_in_client_should_raise_unauthorized(
+        self, app: Flask, client: FlaskClient, setup_item: None
+    ):
+        with app.app_context():
+
+            response: TestResponse = client.get("shopping_cart")
+
+            assert response.status_code == HTTPStatus.UNAUTHORIZED
 
 
 def _get_cookies(cookie_jar: CookieJar | None) -> tuple[Cookie, ...]:
